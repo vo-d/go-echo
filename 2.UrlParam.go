122148c5f42e58d45734a25e18ca81579ff777f4
vo-d <daidaivo2002@gmail.com>
2023-09-30 00:49:55 -0500

    urlparam

--- 2.UrlParam.go
+++ 2.UrlParam.go
@@ -7,25 +7,44 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, world!")
+}
+
+func getCats(c echo.Context) error {
+
+	// get a specific parameter from the url
+	catName := c.QueryParam("name")
+	catType := c.QueryParam("type")
+
+	// Param() will return the datatype of url
+	// ex: http://localhost:8000/cats/string?name=luna&type=yellow
+	dataType := c.Param("data")
+
+	// evaluate the data
+	if dataType == "string" {
+		// %s is parse string
+		return c.String(http.StatusOK, fmt.Sprintf("your cat name is %s\nand his type is: %s\n", catName, catType))
+	}
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"name": catName,
+			"type": catType,
+		})
+	}
+	return c.JSON(http.StatusBadRequest, map[string]string{
+		"error": "you need to lets us know if you want json or string data",
+	})
+
+}
+
 func main() {
 	fmt.Println("Hello to the server")
 
 	e := echo.New()
 
 	e.GET("/", hello)
-	e.GET("/cat", getCats)
+	e.GET("/cats/:data", getCats)
 
 	e.Start(":8000")
-}
-
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, world!")
-}
-
-func getCats(c echo.Context) error {
-	catName := c.QueryParam("name")
-	catType := c.QueryParam("type")
-
-	// %s is parse string
-	return c.String(http.StatusOK, fmt.Sprintf("your cat name is %s\nand his type is: %s\n", catName, catType))
 }
