53c0d6ae272ed6f3f0a4e34fd4d5372420f9b257
vo-d <daidaivo2002@gmail.com>
2023-10-06 20:25:31 -0500

    middleware

--- 4.middleware.go
+++ 4.middleware.go
@@ -1,50 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
-
-type Cat struct {
-	Name string
-	Type string
-}
-
-func addCats(c echo.Context) error {
-	// initialize objects
-
-	cat := Cat{}
-
-	// read the request body and put it to b
-	// use io.ReadAll()
-	b, err := io.ReadAll(c.Request().Body)
-	if err != nil {
-		log.Printf("failed to read the request body: %v", err)
-		return c.String(http.StatusInternalServerError, "")
-	}
-
-	// Unmarshal() decode the request body from the json to a slice, then store it to the address
-	// If &cat is not the address, it will return an error to object. If &cat is the address, it will return nil to object
-	// equivalent to JSON.parse() in ExpressJS
-	object := json.Unmarshal(b, &cat)
-	log.Print(cat)
-	if object != nil {
-		log.Printf("failed to parse the request body: %v", err)
-		return c.String(http.StatusInternalServerError, "")
-	}
-
-	// %#v parse the value in a default format
-	log.Printf("this is your cat: %#v", cat)
-	c.Request().Body.Close()
-
-	return c.String(http.StatusOK, "We got your cat")
-
-}
 
 func mainAdmin(e echo.Context) error {
 	return e.String(http.StatusOK, "you're on the secret admin page")
@@ -56,11 +18,12 @@ func main() {
 	e := echo.New()
 
 	// group here is like the router in ExpressJs
-	g := e.Group("/admin")
+	// this middleware logs the server  interaction
+	g := e.Group("/admin", middleware.Logger())
+	//or g.Use(middleware.Logger())
 
+	// localhost:8000/admin/main
 	g.GET("/main", mainAdmin)
-
-	e.POST("/cats", addCats)
 
 	e.Start(":8000")
 
